e6346d86183fba3b903724d099367d5a5912b55a
Jacob Magnant <jacob@magnant.co>
2019-08-29 11:16:38 -0400

    Formatted code better, added error checking for JSON parse.

--- server.js
+++ server.js
@@ -9,69 +9,54 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
-  //console.log(parsedUrl);
-
   switch(parsedUrl.pathname) {
 
-    case '/':
+    case '/':   //Reserved case for root directory, respond 404 as it is empty for now.
         response.statusCode = 404;
-        response.end("Bad gateway error");
+        response.end("404 - Unknown or unspecified directory.");
       break;
 
-    case '/listings':
+    case '/listings':   //Respond with JSON data for listings directory.
         response.writeHead(200, {"Content-Type": "application/json"});
         response.end(JSON.stringify(listingData, null, 3));
       break;
 
-    default:
+    default:  //Default response 404.
         response.statusCode = 404;
-        response.end("Bad gateway error");
+        response.end("404 - Unknown or unspecified directory.");
 
   }
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+  //Parse data from disk into JSON, check for error from JSON.parse() function.
+  function readFileToJSON() {
+    try {
+      
+      listingData = JSON.parse(data);
+      
+    } catch(e) {
+      
+      console.log(e);
+      return false;
+      
+    }
+    return true;
+  }
 
-    HINT: Read up on JSON parsing Node.js
-   */
+  //Only create & start server if parsing JSON was successful.
+  if(readFileToJSON){
+    
+    // Create Server
+    server = http.createServer(requestHandler);
+    
+    // Start Server
+    server.listen(port, function() {
+      console.log('Server listening on: http://127.0.0.1:' + port);
+    });
 
-    listingData = JSON.parse(data);
-
-    //Check for errors
-  
-
-   //Save the sate in the listingData variable already defined
-  
-
-  // Create Server
-  server = http.createServer(requestHandler);
-
-  // Start Server
-  server.listen(port, function() {
-    console.log('Server listening on: http://127.0.0.1:' + port);
-  });
-
+  }
 
 });
